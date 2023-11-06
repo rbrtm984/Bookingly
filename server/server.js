690e97bc30f6c19a6bbc6cf35b52fc470f63b5ee
margaritabizhan <margaritabizhan@gmail.com>
2023-11-06 16:37:27 -0500

    progress

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
