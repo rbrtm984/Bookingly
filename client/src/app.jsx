5838998da5a96edf70f060e0f81d3c4ccb7ffede
Brooke Sauro <bhsauro@gmail.com>
2023-11-06 17:37:01 -0500

    reconfigured tailwind config to allow for form template formatting, added template signin jsx file to use for testing cookies

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import './style.css'
+import SignIn from "./signin.jsx";
+import SignUp from "./signup.jsx";
+
 
 const App = () => {
     return (
-        <>
-         <h1 class="text-3xl underline">
-            Code and Kart! 
-        </h1>
-        </>
+        <SignUp />
     )
 }
 
