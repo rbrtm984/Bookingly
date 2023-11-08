5dae20974bf3373f88206d2918bc6e17b2fbfd84
margaritabizhan <margaritabizhan@gmail.com>
2023-11-08 11:47:55 -0500

    add participant to the race

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
+app.use(express.json());
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
